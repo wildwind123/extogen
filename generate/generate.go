3c98e47dccfd66ed2614766a8cfc9ccb2fba2f69
Petr <vinokurovvin@list.ru>
2024-10-11 11:10:32 +0900

    last commit

--- generate/generate.go
+++ generate/generate.go
@@ -1,3 +1,3 @@
 package generate
 
-//go:generate go run github.com/wildwind123/extogen/cmd/main@latest --source_dir fixture
+//go:generate go run github.com/wildwind123/extogen/cmd/main@0b8d6c90f710f15c132b17f181b6476946450832 --source_dir fixture
