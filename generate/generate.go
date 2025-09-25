44c08705c3fe67b895535af20686ee77faba49ef
Petr <vinokurovvin@list.ru>
2025-09-25 15:53:22 +0900

    fixed

--- generate/generate.go
+++ generate/generate.go
@@ -1,3 +1,3 @@
 package generate
 
-//go:generate go run github.com/wildwind123/extogen/cmd/main@4389deccf38fd9b94757e89dc2033a4305a18eb0 --source_dir fixture
+//go:generate go run github.com/wildwind123/extogen/cmd/main@9eda138df32a65b7be4e9d99b64f94f36960afc8 --source_dir fixture
