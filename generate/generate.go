cd0201d2da76e4b5460b9016363b9bcd11bde8f9
ganbatte <vinokurovvin@list.ru>
2025-12-21 18:29:48 +0900

    ToPointer

--- generate/generate.go
+++ generate/generate.go
@@ -1,3 +1,3 @@
 package generate
 
-//go:generate go run github.com/wildwind123/extogen/cmd/main@9eda138df32a65b7be4e9d99b64f94f36960afc8 --source_dir fixture
+//go:generate go run github.com/wildwind123/extogen/cmd/main@b9f143929fa99c46bfb941cee112b973d73eb516 --source_dir fixture
